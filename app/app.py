bb5a08f4e9dc269dfe53585f13e163dc222752ca
Guillaume Marchand <gmarchan@amazon.fr>
2019-01-15 10:05:19 +0100

    Add route to flask

--- app/app.py
+++ app/app.py
@@ -3,10 +3,20 @@ import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello Flask"
+# List all items of the DynamoDB Table
+@app.route("/", methods=["GET"])
+def list():
+    return "Hello GET"
 
+# Get an item from the DynamoDB table
+@app.route("/<name>", methods=["GET"])
+def get(name):
+    return "GET Hello %s" %(name)
+
+# Post a new Item to the DynamoDB table
+@app.route("/<name>", methods=["POST"])
+def post(name):
+    return "POST Hello %s" %(name)
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
