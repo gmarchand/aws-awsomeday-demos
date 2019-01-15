4df6eee3f2093798275b034894b9ced278f2da51
Guillaume Marchand <gmarchan@amazon.fr>
2019-01-15 22:46:52 +0100

    Adding aws auth to docker compose

--- app/app.py
+++ app/app.py
@@ -1,12 +1,19 @@
+from hello.hello import HelloYou
 from flask import Flask
 import os
+import boto3
+import json
 
 app = Flask(__name__)
+
+session = boto3.session.Session(region_name="eu-west-1")
+hello = HelloYou(session, 'aws-awsomeday-api-HelloTable-13QO3YN75QYQB')
 
 # List all items of the DynamoDB Table
 @app.route("/", methods=["GET"])
 def list():
-    return "Hello GET"
+    result = hello.list()
+    return json.dumps(result)
 
 # Get an item from the DynamoDB table
 @app.route("/<name>", methods=["GET"])
