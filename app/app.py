aa38e0d9a68fa681ab6896d3e0d2f51415c887f4
Guillaume Marchand <gmarchan@amazon.fr>
2019-01-30 15:59:36 +0100

    Debug MU

--- app/app.py
+++ app/app.py
@@ -13,19 +13,26 @@ import json
 app = Flask(__name__)
 
 session = boto3.session.Session()
-hello = HelloYou(session, os.environ['TABLE_NAME'])
+#hello = HelloYou(session, os.environ['TABLE_NAME'])
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 # Get an item from the DynamoDB table
 @app.route("/<name>", methods=["GET"])
 def get(name):
     result = hello.get(name)
+    logger.info('Hello %s from Docker !', name)
     return json.dumps(result)
 
 # Post a new Item to the DynamoDB table
 @app.route("/<name>", methods=["POST"])
 def post(name):
     result = hello.post(name)
+    logger.info('Hello %s from Docker !', name)
     return json.dumps(result)
+
+# Health check for ECS Fargate
 @app.route("/", methods=["GET"])
 def healthcheck():
     return '{"health": "OK"}'
